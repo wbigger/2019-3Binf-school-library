3eef22e8e0520a7c046e2ecf511806beacae5517
Claudio Capobianco <claudio.capobianco@istruzione.it>
2019-10-12 09:53:29 +0200

    add init function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 class Book:
-    title = "no title"
-    author = "no author"
-    sbn = 0
+    def __init__(self, title, author, sbn):
+        self.title = title
+        self.author = author
+        self.sbn = sbn
 
-harry_potter = Book()
+harry_potter = Book("Harry Potter e il calice di fuoco", "J.K.Rowling",1)
 print(harry_potter.title)
 
+
+
+#piccolo_principe = Book()
+#print(piccolo_principe.title)
